391f78629c838819e54a0ebd99fbbc7836cbd104
stefimaz <89653430+stefimaz@users.noreply.github.com>
2022-03-24 11:21:08 -0400

    navigation bar file added

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -1,8 +1,31 @@
 
 import logo from './logo.png';
 import './App.css';
- 
+
+import { useState } from 'react'
+import { ethers } from "ethers"
+
 function App() {
+
+  const [account, setAccount] = useState(null)
+
+  const web3Handler = async () => {
+    // Metamask connect/login
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    setAccount(accounts(0))
+    // get procvider from Metamask
+    const provider = new ethers.providers.web3Handler(window.ethereum)
+    //set signer
+    const signer = provider.getSingner()
+
+    loadContract(signer)
+  }
+  const loadContract = async (signer) => {
+    const marketplace = new ethers.Contract(MarketplaceAddress.address, MarketplaceAbi.abi, signer)
+    setMarkeplace(marketplace)
+    const nft = new ethers.Contract(NFTAddress.address, NFTAbi.abi, signer)
+    setNFT(nft)
+  }
   return (
     <div>
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
@@ -24,9 +47,9 @@ function App() {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <img src={logo} className="App-logo" alt="logo"/>
+                <img src={logo} className="App-logo" alt="logo" />
               </a>
-              <h1 className= "mt-5">Dapp University Starter Kit</h1>
+              <h1 className="mt-5">Dapp University Starter Kit</h1>
               <p>
                 Edit <code>src/frontend/components/App.js</code> and save to reload.
               </p>
